03c2c887000df76574b75eb69f9ce6a944d396c8
wtkn25 <atrastar72@gmail.com>
2021-01-29 01:47:05 +0900

    ECアプリ作成

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'EC.apps.EcConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
